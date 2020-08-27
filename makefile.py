6e42a77af9cd9eb71de7de21ce450d1cef247d28
bigfoxtail <bigfoxtail@outlook.com>
2020-08-27 20:42:20 +0800

    使用UTC时间

--- makefile.py
+++ makefile.py
@@ -13,10 +13,9 @@ if os.path.exists('bing_wallpaper'):
     shutil.rmtree('bing_wallpaper')
 os.mkdir('bing_wallpaper')
 
-
-print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-c_year = time.strftime("%Y", time.localtime())
-c_monthdate = time.strftime("%m%d", time.localtime())
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+c_year = time.strftime("%Y", time.gmtime())
+c_monthdate = time.strftime("%m%d", time.gmtime())
 version = "1." + c_year + "." + c_monthdate
 
 bing_wallpaper = '{"response":{"protocol":"3.1","server":"prod","app":[{"appid":"gccbbckogglekeggclmmekihdgdpdgoe","status":"ok","updatecheck":{"status":"ok","urls":{"url":[{"codebase":"https://github.com/bigfoxtail/bing_wallpaper/releases/latest/download/bing_wallpaper.crx"}]},"manifest":{"version":"1.0.0","packages":{"package":[{"name":"bing_wallpaper.crx","hash_sha256":"ba8b38b3d9eb556cc3118f75453a59219436358f38160dabee80234335ce57ed","required":true}]}}}}]}}'
